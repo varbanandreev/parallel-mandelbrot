27d56419e48cd1f5fced3a1c19d4f9cd979a83af
Varban Andreev <varban.andreev@gmail.com>
2019-06-09 20:37:14 +0300

    Parse command line options

---
+++ ParallelMandelbrot/src/main/Main.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Options options = readArgs(args);
+    }
+
+    private static Options readArgs(String[] args) {
+        Map<String, String> inputOptions = new HashMap<>();
+        Options options = new Options();
+
+        try {
+            for (int i = 0; i < args.length; i++) {
+                switch (args[i]) {
+                    case "-s":
+                    case "-size":
+                        inputOptions.put("size", args[++i]);
+                        break;
+                    case "-r":
+                    case "-rect":
+                        inputOptions.put("rect", args[++i]);
+                        break;
+                    case "-t":
+                    case "-tasks":
+                        inputOptions.put("tasks", args[++i]);
+                        break;
+                    case "-o":
+                    case "-output":
+                        inputOptions.put("output", args[++i]);
+                        break;
+                    case "-q":
+                    case "-quiet":
+                        inputOptions.put("quiet", args[++i]);
+                        break;
+                }
+            }
+
+            options.parseOptions(inputOptions);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Invalid options format!");
+        }
+
+        return options;
+    }
+}
---
+++ ParallelMandelbrot/src/main/Options.java
@@ -0,0 +1,68 @@
+package main;
+
+import java.util.Map;
+
+public class Options {
+    private int pictureWidth;
+    private int pictureHeight;
+    private double planeStartX;
+    private double planeEndX;
+    private double planeStartY;
+    private double planeEndY;
+    private int threadCount;
+    private String fileName;
+    private boolean isQuiet;
+
+    public int getPictureWidth() {
+        return pictureWidth;
+    }
+
+    public int getPictureHeight() {
+        return pictureHeight;
+    }
+
+    public double getPlaneStartX() {
+        return planeStartX;
+    }
+
+    public double getPlaneEndX() {
+        return planeEndX;
+    }
+
+    public double getPlaneStartY() {
+        return planeStartY;
+    }
+
+    public double getPlaneEndY() {
+        return planeEndY;
+    }
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public boolean isQuiet() {
+        return isQuiet;
+    }
+
+    public void parseOptions(Map<String, String> options) {
+        String optionValue = options.getOrDefault("size", "640x480");
+        String[] optionValueSplit = optionValue.split("x");
+        this.pictureWidth = Integer.parseInt(optionValueSplit[0]);
+        this.pictureHeight = Integer.parseInt(optionValueSplit[1]);
+
+        optionValueSplit = options.getOrDefault("rect", "-2.0:2.0:-2.0:2.0").split(":");
+        this.planeStartX = Double.parseDouble(optionValueSplit[0]);
+        this.planeEndX = Double.parseDouble(optionValueSplit[1]);
+        this.planeStartY = Double.parseDouble(optionValueSplit[2]);
+        this.planeEndY = Double.parseDouble(optionValueSplit[3]);
+
+        this.threadCount = Integer.parseInt(options.getOrDefault("tasks", "1"));
+        this.fileName = options.getOrDefault("output", "zad16.png");
+        this.isQuiet = Boolean.parseBoolean(options.getOrDefault("quiet", "false"));
+    }
+}

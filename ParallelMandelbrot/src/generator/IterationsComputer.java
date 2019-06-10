22ff557b370af432b9d277712c219d710f7f93ca
Varban Andreev <varban.andreev@gmail.com>
2019-06-10 14:16:53 +0300

    Add iterations computer

---
+++ ParallelMandelbrot/src/generator/IterationsComputer.java
@@ -0,0 +1,33 @@
+package generator;
+
+public class IterationsComputer {
+    private static int ESCAPE_NUMBER = 1 << 16;
+
+    private int maxIterations;
+
+    public IterationsComputer(int maxIterations) {
+        this.maxIterations = maxIterations;
+    }
+
+    public void computeIterations(MandelbrotNumber number) {
+        double x = 0;
+        double y = 0;
+        int iterations = 0;
+
+        while (iterations < this.maxIterations && !hasEscaped(x, y)) {
+            double nextX = x * x - y * y + number.getRealPart() * Math.pow(Math.E, -x) * Math.cos(-y);
+            double nextY = 2 * x * y + number.getImaginaryPart() * Math.pow(Math.E, -x) * Math.sin(-y);
+
+            x = nextX;
+            y = nextY;
+
+            iterations++;
+        }
+
+        number.setIterations(iterations);
+    }
+
+    private boolean hasEscaped(double x, double y) {
+        return Math.sqrt(x * x + y * y) > ESCAPE_NUMBER;
+    }
+}
